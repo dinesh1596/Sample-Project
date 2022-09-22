1f2e3e9209b19b9520f4bf1c81da8d033f2d61e3
dinesh1596 <dinesharasu1596@gmail.com>
2022-09-22 16:11:52 -0700

    newcode

---
+++ src/org/demo/Task.java
@@ -0,0 +1,16 @@
+package org.demo;
+
+
+
+public class Task {
+	public static void main(String[] args) {
+		int empId=90;
+		if(empId>=100) {
+			System.out.println("valid");
+		}else {
+			System.out.println("not valid");
+		}
+		
+
+}
+}
